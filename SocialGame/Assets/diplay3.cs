7751040493e83e95ecd3eeae474a8603ae58de4b
bobwzb <42678607+bobwzb@users.noreply.github.com>
2019-05-12 21:40:44 -0700

    text

---
+++ SocialGame/Assets/diplay3.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class diplay3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] m;
+    public int flag;
+    public Vector3 k;
+    void Start()
+    {
+        for(int i = 0; i <= 6; i++)
+        {
+            m[i].SetActive(false);
+            k = this.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m[0].SetActive(true);
+        if (flag <= 6)
+        {
+            if (m[flag].GetComponent<nextscentence>().end == true)
+            {
+                flag++;
+                if (flag <= 6)
+                {
+                    m[flag].SetActive(true);
+                }
+                if (flag >= 3)
+                {
+                    k = new Vector3(this.transform.position.x, this.transform.position.y + 1.5f, this.transform.position.z);
+                }
+            }
+        }
+        this.transform.position = Vector3.MoveTowards(this.transform.position, k, 2 * Time.deltaTime);
+    }
+}
---
+++ SocialGame/Assets/nextscentence.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nextscentence : MonoBehaviour
+{
+    public char[] a;
+    public char[] b;
+    public float curtime;
+    public int flag;
+    public bool end = false;
+    public Vector3 k;
+    void Start()
+    {
+        a = this.GetComponent<TextMesh>().text.ToCharArray();
+        b= this.GetComponent<TextMesh>().text.ToCharArray();
+        for(int i=0;i<= a.Length-1; i++)
+        {
+            b[i] = ' ';
+        }
+    }
+    void Update()
+    {
+        if (curtime == 0)
+        {
+            curtime = Time.time;
+        }
+        if (Time.time - curtime > 0.05&&flag<=a.Length-1)
+        {
+            b[flag] = a[flag];
+            flag++;
+            curtime = 0;
+            if(flag>= a.Length - 1)
+            {
+                end = true;
+            }
+        }
+        string m = new string(b);
+        this.GetComponent<TextMesh>().text = m;
+    }
+
+}
