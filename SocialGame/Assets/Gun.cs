ded51e5d93eb9bb58fb3e33b19736714559464d6
bobwzb <42678607+bobwzb@users.noreply.github.com>
2019-05-14 17:15:54 -0700

    grouptalk

---
+++ SocialGame/Assets/Gun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float curtime;
+    private Vector3 k;
+    void Start()
+    {
+        k = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("Talk").GetComponent<diplay3>().start == true)
+        {
+                if (GameObject.Find("Talk").GetComponent<diplay3>().move == true)
+                {
+                    k = new Vector3(this.transform.position.x, this.transform.position.y+2.7f, this.transform.position.z);
+                    GameObject.Find("Talk").GetComponent<diplay3>().move = false;
+                }
+        }
+        this.transform.position = Vector3.MoveTowards(this.transform.position, k, 2.5f * Time.deltaTime);
+
+    }
+}
--- SocialGame/Assets/Script/diplay3.cs
+++ SocialGame/Assets/Script/diplay3.cs
@@ -7,7 +7,11 @@ public class diplay3 : MonoBehaviour
     // Start is called before the first frame update
     public GameObject[] m;
     public int flag;
+    public float curtime;
+    public float wtime;
     public Vector3 k;
+    public bool start=false;
+    public bool move = false;
     void Start()
     {
         for(int i = 0; i <= m.Length-1; i++)
@@ -15,27 +19,52 @@ public class diplay3 : MonoBehaviour
             m[i].SetActive(false);
             k = this.transform.position;
         }
+        curtime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        m[0].SetActive(true);
-        if (flag <= m.Length - 1)
+        if (Time.time - curtime >= 1.8f)
         {
-            if (m[flag].GetComponent<nextscentence>().end == true)
+            start = true;
+        }
+        if (this.name == "tex")
+        {
+            start = true;
+        }
+        if (start == true)
+        {
+                m[0].SetActive(true);
+            if (flag <= m.Length - 1)
             {
-                flag++;
-                if (flag <= m.Length - 1)
+                if (m[flag].GetComponent<nextscentence>().end == true)
                 {
+                    flag++;
                     m[flag].SetActive(true);
-                }
-                if (flag >= 3)
-                {
-                    k = new Vector3(this.transform.position.x, this.transform.position.y + 1.7f, this.transform.position.z);
+                    if (flag <= m.Length - 1)
+                    {
+                            m[flag].SetActive(true);
+                    }
+                    if (flag >= 1&&flag<=1)
+                    {
+                        k = new Vector3(this.transform.position.x, this.transform.position.y + 2.7f, this.transform.position.z);
+                        move = true;
+                    }
+                    else if (flag >= 2)
+                    {
+                        k = new Vector3(this.transform.position.x, this.transform.position.y + 1.7f, this.transform.position.z);
+                        move = true;
+                    }
                 }
             }
+            this.transform.position = Vector3.MoveTowards(this.transform.position, k, 2.5f * Time.deltaTime);
         }
-        this.transform.position = Vector3.MoveTowards(this.transform.position, k, 2 * Time.deltaTime);
+    }
+    IEnumerator waitAnimation3()
+    {
+        yield return new WaitForSeconds(3f);
+        flag++;
+
     }
 }
